1f53631ae995a41c827849b79f66633ea86738be
Илья Гедройц <i.gedroyts@interneturok.ru>
2023-04-30 19:43:39 +0600

    Добавление сохранения сообщения в базу данных

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -30,6 +36,7 @@ func main() {
 
 	// Обработчики команд бота
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(onReceiveMessage)
 
 	// Открываем сессию
 	err = dg.Open()
@@ -62,4 +69,55 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.ChannelID == os.Getenv("TEST_CHANNEL_ID") {
 		s.ChannelMessageSend(m.ChannelID, "Привет, я пидорас!")
 	}
+}
+
+func onReceiveMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Игнорируем сообщения от ботов и себя
+	if m.Author.Bot || m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Устанавливаем соединение с сервером
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://ebobot_db_1:27017"))
+	if err != nil {
+		fmt.Println("Не создать клиент базы данных: ", err)
+	}
+
+	// Подключаем клиента к серверу
+	ctx := context.Background()
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println("Не удалось присоединиться к базе данных: ", err)
+	}
+
+	// Определяем имя базы данных и коллекции
+	db := client.Database("ebobot")
+	col := db.Collection("sentences")
+
+	// Очищаем предложение
+	cleanSentence := sanitizeSentence(m.Content)
+
+	if cleanSentence == "" {
+		return
+	}
+
+	// Создаем новый документ, содержащий очищенное предложение
+	doc := bson.D{{Key: "sentence", Value: cleanSentence}}
+
+	// Вставляем документ в коллекцию
+	_, err = col.InsertOne(ctx, doc)
+	if err != nil {
+		fmt.Println("Не удалось сохранить запись в коллекцию: ", err)
+	}
+}
+
+func sanitizeSentence(sentence string) string {
+	// Удаляем символы, отличные от букв и цифр
+	regex := regexp.MustCompile(`<@?[:[a-zA-Z0-9]*]*>|![a-zA-Z]*|http\S+`)
+	sentence = regex.ReplaceAllString(sentence, " ")
+
+	// Приводим к нижнему регистру и удаляем лишние пробелы
+	sentence = strings.ToLower(strings.TrimSpace(sentence))
+
+	return sentence
 }
